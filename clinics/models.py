25be89c6ef0ee327abcb2dc09f99ea66de75f892
MarAlba8 <driadesm8@gmail.com>
2023-06-05 00:40:53 -0400

    feat(clinics): remove code to create clinics

--- clinics/models.py
+++ clinics/models.py
@@ -18,29 +18,3 @@ class Clinic(models.Model):
 
     def __str__(self):
         return self.name
-
-try:
-
-    a=Clinic.objects.all()
-    if(len(a)>6):
-        print("### YA HAY clinicas registradas ####")
-    
-    else:
-        print("### NO HAY ####")
-        clinic1 = Clinic.create("Integral del Adulto")
-        clinic2 = Clinic.create("Estomatología")
-        clinic3 = Clinic.create("Cirugia")
-        clinic4 = Clinic.create("Operatoria Dental")
-        clinic5 = Clinic.create("Endodoncia")
-        clinic6 = Clinic.create("Periodoncia")
-
-
-        clinic1.save()
-        clinic2.save()
-        clinic3.save()
-        clinic4.save()
-        clinic5.save()
-        clinic6.save()
-
-except:
-    print("err235")
